80bb9c30477c45e6a34d008285c84556e6af87f3
Hannah Joseph <154567528+hannah-joseph@users.noreply.github.com>
2025-03-03 11:33:04 -0500

    Update baseMap.js

--- templates/js/baseMap.js
+++ templates/js/baseMap.js
@@ -13,4 +13,13 @@ var openStreetMap = L.tileLayer(
 // Add the OpenStreetMap tile layer to the map
 openStreetMap.addTo(mymap);
 
+//Comment out the code below if app is not County app
+var mapboxSatelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWdzbWl0aCIsImEiOiJjbHRzeHNmdGIwdGViMmprOG5keXlrbGYwIn0.QeV2H_Y4QN8AFQ5B10JnqA', {
+    attribution: 'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a> | <a href = "https://forms.office.com/r/NEcbte1TLB" target="_blank">Feedback</a> | <a href="https://www.youtube.com/watch?v=guCxeNT-YqA", target="_blank">About</a> | <a href="images/pestHubMap_HelpFile.pdf", target="_blank">Help</a>',
+    subdomains: ['a','b','c'],
+    tileSize: 512,
+    maxZoom: 20,
+    zoomOffset: -1
+});
 
+mapboxSatelliteStreets.addTo(mymap);
