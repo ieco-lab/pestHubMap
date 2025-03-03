8f674887624c3c4ab9c99efcb08cea56537f778a
Hannah Joseph <154567528+hannah-joseph@users.noreply.github.com>
2025-03-03 11:45:25 -0500

    Update baseMap.js

--- templates/js/baseMap.js
+++ templates/js/baseMap.js
@@ -1,3 +1,5 @@
+
+//Comment out the code below if the app is a County app
 // Create an OpenStreetMap tile layer using Leaflet
 var openStreetMap = L.tileLayer(
     'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', // Tile URL pattern
@@ -13,7 +15,7 @@ var openStreetMap = L.tileLayer(
 // Add the OpenStreetMap tile layer to the map
 openStreetMap.addTo(mymap);
 
-//Comment out the code below if app is not County app
+//Comment out the code below if the app is not County app
 var mapboxSatelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWdzbWl0aCIsImEiOiJjbHRzeHNmdGIwdGViMmprOG5keXlrbGYwIn0.QeV2H_Y4QN8AFQ5B10JnqA', {
     attribution: 'Map data &copy; <a href="https://www.mapbox.com/">Mapbox</a> | <a href = "https://forms.office.com/r/NEcbte1TLB" target="_blank">Feedback</a> | <a href="https://www.youtube.com/watch?v=guCxeNT-YqA", target="_blank">About</a> | <a href="images/pestHubMap_HelpFile.pdf", target="_blank">Help</a>',
     subdomains: ['a','b','c'],
